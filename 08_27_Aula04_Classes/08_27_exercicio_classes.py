5c2be615a60857d4c1578d27c3858d710a5a130b
fortesvitoria <fortesvitoria@gmail.com>
2025-08-27 21:29:46 -0300

    add aula 27 agosto 2025

---
+++ 08_27_Aula04_Classes/08_27_exercicio_classes.py
@@ -0,0 +1,133 @@
+'''
+Exercício parte 1
+Crie uma classe Porta com os atributos: altura, largura, cor.
+Crie os métodos para pintar, abrir e fechar a porta.
+
+Exercício parte 2
+Crie uma classe Fechadura com a propriedade chave (chave será o segredo).
+Crie os métodos de chavear e deschavear.
+
+Na classe porta adicione o atributo fechadura. Crie o método instalar_fechadura.
+Agora crie os métodos de trancar e destrancar a porta com a chave.
+
+Você deverá conseguir abrir e fechar a porta se esta estiver destrancada.
+Você não conseguirá abrir ou fechar a porta se está já estiver previamente trancada.
+'''
+from pyexpat import features
+
+
+class Fechadura:
+    # abaixo atributos de instancia, especificos do objeto
+    def __init__(self,segredo):  # cria construtor, nao precisa informar os parametros se todos objetos instanciados forem iguais
+        self.chave = segredo
+        self.chaveada = False #deschaveado
+
+    def chave_correta(self, chave):
+        if chave == self.chave:
+            return True
+        return False
+
+    #metodo para chavear
+    def chavear(self, chave):
+        if chave == self.chave:
+            self.chaveada = True #porta chaveada
+
+    # metodo para deschavear
+    def deschavear(self, chave):
+        if chave == self.chave:
+            self.chaveada = False #dechaveada
+
+    #
+    def esta_chaveada(self):
+        if self.chaveada:
+            return 'Chaveada'
+        return 'Deschaveada'
+
+    def converte(self):
+        if self.chaveada:
+            return "CHAVEADA"
+        return "DESCHAVEADA"
+
+    def __str__(self):
+        return f'Fechadura: {self.converte()}'
+
+class Porta:
+    marca = "Portas SA" #atributos de classe, todos objetos o terão
+    #abaixo atributos de instancia, especificos do objeto
+    def __init__(self, largura=60): #cria construtor, nao precisa informar os parametros se todos objetos instanciados forem iguais
+        self.modelo = "A001"
+        self.altura = 220
+        self.largura = largura
+        self.fechadura = None
+        self.cor = None
+        self.aberta = True #se true, a porta está aberta
+
+    #define nova cor
+    def pintar(self, nova_cor):
+        self.cor = nova_cor
+
+    #metodo para abrir a porta
+    def abrir(self):
+        if not self.fechadura or not self.fechadura.esta_chaveada():
+            self.aberta = True
+
+    #metodo para fechar a porta
+    def fechar(self):
+        if not self.fechadura or not self.fechadura.esta_chaveada():
+            self.aberta = False
+
+    #instalar fechadura
+    def instalar_fechadura(self,fechadura):
+        self.fechadura = fechadura
+        self.aberta = True
+
+    #Agora crie os métodos de trancar e destrancar a porta com a chave.
+    def trancar(self, chave):
+        if self.fechadura and self.fechadura.chave_correta(chave):
+            self.fechadura.chavear(chave)
+
+    def destrancar(self, chave):
+        if chave == False:
+            return 'A porta já está destrancada'
+        return 'Destrancada'
+
+
+    def converte(self): #Função para exibir outra mensagem ao inves de true ou false. Se for true retorna aberta, caso contrario fechada
+        if self.aberta:
+            return "ABERTA"
+        return "FECHADA"
+
+    def __str__(self): #utilizado para mascarar o endereço do abjeto com uma mascara de string
+        return f'''
+        - Marca como atributo instancia: {self.marca}
+        - Marca como atributo de classe: {Porta.marca}
+        
+        Altura: {self.altura} - Largura: {self.largura}
+        Cor: {self.cor} - Status: {self.converte()}
+        
+        Trancada - {self.trancar()}
+        Destrancada - {self.destrancar()}
+    
+        '''
+
+################################
+p1 = Porta(90)
+p1.pintar('Azul')
+p1.fechar()
+
+f1 = Fechadura('1234')
+
+p1.instalar_fechadura(f1) #unindo dois objetos = agregação
+p1.fechar()
+p1.trancar('1234')
+p1.abrir()
+p1.destrancar('1234')
+
+p2 = Porta()
+p2.trancar = False
+p2.pintar('Vermelha')
+p2.marca = "Portinhas SA" #atributo de tempo de excuçao, gruda no objeto quando o codigo roda, esse marca nao tem a ver com a marca de classe
+
+print(p1)
+print(p2)
+
