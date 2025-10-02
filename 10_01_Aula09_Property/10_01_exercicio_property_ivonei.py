f9602324be5687f28696036174cc5429eeeb0c6e
fortesvitoria <fortesvitoria@gmail.com>
2025-10-01 21:25:09 -0300

    add aula

--- 10_01_Aula09_Property/10_01_exercicio_property_ivonei.py
+++ 10_01_Aula09_Property/10_01_exercicio_property_ivonei.py
@@ -36,6 +36,34 @@ Adicionar uma classe Sensor que simula leituras aleatórias.
 '''
 import random
 
+class Estufa:
+    def __init__(self):
+        self.__temperatura = 20
+        self.__alertas_temperatura = 0
+
+    @property
+    def temperatura(self):
+        return self.__temperatura        
+
+    @temperatura.setter
+    def temperatura(self, valor):
+        if 15 <= valor <= 35:
+            self.__temperatura = valor
+        else:
+            raise ValueError (f"Temperatura {valor}C está fora do intervalo.")
+
+    def status (self):
+        if 20 <= self.__temperatura <= 30:
+            self.__alertas_temperatura = 0
+            return f"Temperatura de {self.__temperatura}C, está ideal."
+        else:
+            self.__alertas_temperatura += 1
+            return f"Temperatura {self.__temperatura}C está fora do ideal."
+
+    def alerta(self):
+        if self.__alertas_temperatura >= 3:
+            return f"ALERTA!!! Temperatura {self.__temperatura}C está fora do ideal por 3 vezes consecutivas!"
+
 class Sensor:
     def __init__(self):
         self.__value = random.randint(0,50)
@@ -44,27 +72,12 @@ class Sensor:
     def value(self):
         return self.__value
 
-s = Sensor()
+def simular_estufa():
+    s = Sensor()
+    n = Estufa(s.value)
 
-class Estufa:
-    def __init__(self, temperatura):
-        self.__temperatura = temperatura
 
-    @property
-    def temperatura(self):
-        return self.__temperatura
-    
-    def status (self):
-        if self.__temperatura >= 20 and self.__temperatura <= 30:
-            print(f"Temperatura de {self.__temperatura}C, está ideal.")
-        else:
-            print(f"Temperatura {self.__temperatura}C está fora do ideal.")
-
-    @temperatura.setter
-    def temperatura(self, nova_temperatura):
-        self.__temperatura = nova_temperatura
-        self.status
-
-n = Estufa(s.value)
-n.status()
-        
+for i in range (10):
+    try: 
+        leitura = sensor.ler_temperatura()
+        estufa.temperatura = leitura
