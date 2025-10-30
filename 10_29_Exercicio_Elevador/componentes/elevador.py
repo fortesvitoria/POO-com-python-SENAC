2fa883c3dff5532bf9d830fbb2bfe3a8fd3e0e50
Vitoria Fortes <fortesvitoria@gmail.com>
2025-10-30 11:48:13 -0300

    finaliza trabalho elevador

--- 10_29_Exercicio_Elevador/componentes/elevador.py
+++ 10_29_Exercicio_Elevador/componentes/elevador.py
@@ -1,24 +1,40 @@
+# 1 Cria classe elevador
 class Elevador:
+    # 1.1 Cria construtor, que recebe como parametro um andar
     def __init__(self, andar = 1):
         self.__andar = andar
     
+    # 1.2 Cria uma metodo para mover o elevalor, que retorna uma string
+    '''
+    Metodo locomover(destino) recebe como parametro um destino  e retorna uma string(mensagem)
+    ele recebe um destino em formato int que verifica para onde o usuario quer ir
+    1. Se o destido for maior ou igual a um e menor ou igual a 15:
+        1.1 Se andar for igual a destino, informa que já está no andar
+        1.2 Se o destino for igual a um, informa que está indo para o térreo;
+        1.3 Senão, informa o andar para o qual está indo.
+    2. Se não for menor que um ou maior que 15, retorna mensagem de erro
+    '''
     def locomover(self, destino) -> str:
         destino = int(destino)
         if destino >= 1 and destino <= 15:
-            if destino == self.__andar:
+            if destino == 1 and self.__andar ==1:
+                return self.__mensagem_esta_no_terreo()
+            elif destino == self.__andar:
                 return self.__mensagem_mesmo_andar(destino)
             elif destino == 1 and self.__andar != 1:
                 self.__andar = destino
                 return self.__mensagem_alteracao_terreo()
-            else:
+            elif destino != self.__andar:
                 self.__andar = destino
                 return self.__mensagem_alteracao_andar(destino)
         else:
             return self.__mensagem_erro()
     
+    # 1.3 Metodo para pegar o andar, retorna um int
     def get_andar(self) -> int:
         return self.__andar
-
+    
+    # 1.4 Metodos com mensagens de alerta e de erro, retornam uma string
     def __mensagem_erro(self) -> str:
         return "Andar inválido! Digite um valor entre 1 e 15."
     
@@ -30,5 +46,9 @@ class Elevador:
     
     def __mensagem_mesmo_andar(self, andar) -> str:
         return f"Você já está no andar {andar}."
+    
+    def __mensagem_esta_no_terreo(self) -> str:
+        return "Você está no térreo."
+
 
 
--- 10_29_Exercicio_Elevador/run.py
+++ 10_29_Exercicio_Elevador/run.py
@@ -1,12 +1,45 @@
 from componentes.elevador import *
+from flask import Flask, render_template, request
 
-def main():
-        elevador = Elevador() #cria uma instancia 
-        print(elevador.get_andar())
-        print(elevador.locomover(4))
-        print(elevador.get_andar())
-        print(elevador.locomover(15))
+# def main():
+#         elevador = Elevador() #cria uma instancia 
+#         print(elevador.get_andar())
+#         print(elevador.locomover(4))
+#         print(elevador.get_andar())
+#         print(elevador.locomover(15))
 
-# Programa principal
-if __name__ == "__main__":
-    main()
+# # Programa principal
+# if __name__ == "__main__":
+#     main()
+
+# 1 Inicia flask
+app = Flask(__name__, static_folder='static')
+# 1.2 Chave secreta necessária para inicar sessao
+app.secret_key = 'secret-key-guesss'
+
+# 2 Cria instancia do elevador
+elevador = Elevador()
+
+# 3. Rota para elevador
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    #incia com mensagem informando que está no térreo
+    mensagem = ''
+
+    # 3.1 Se houver envio de formulario (clique no botao)
+    if request.method == 'POST':
+        # 3.1.1 Pega o andar do destino value=""
+        destino = request.form['destino']
+        # 3.1.2 Chama a logica da classe Elevador
+        mensagem = elevador.locomover(destino)
+    # 3.2 Pega o andar atual
+    andar_atual = elevador.get_andar()
+    # 3.3 Renderiza(atualiza) a página com a mensagem e o andar atual
+    return render_template('interface_elevador.html', mensagem=mensagem, andar_atual=andar_atual)
+
+# 4. Inicia aplicação
+# --- Run the App ---
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
